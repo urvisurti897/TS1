bc544c403b3f1044045a506d7cd5cbb7c39dd4af
Urvi <urvisurti897@gmail.com>
2025-03-12 12:02:32 -0400

    Added testcases for validating values for zero

--- src/calculateVolumeOfPrism.test.ts
+++ src/calculateVolumeOfPrism.test.ts
@@ -31,11 +31,40 @@ describe("calculate the volume of a prism", () => {
 
 
   test("should throw an error when height is negative", () => {
-    const width = -2;
+    const width = 2;
+    const length = 3;
+    const height = -5;
+
+    expect(() => calculateVolumeOfaPrism(width, length, height)).toThrow("Height should be positive");
+
+  });
+
+
+  test("should throw an error when length is zero", () => {
+    const width = 2;
+    const length = 0;
+    const height = 5;
+
+    expect(() => calculateVolumeOfaPrism(width, length, height)).toThrow("Length should not be zero");
+
+  });
+
+  test("should throw an error when width is zero", () => {
+    const width = 0;
     const length = 3;
     const height = 5;
 
-    expect(() => calculateVolumeOfaPrism(width, length, height)).toThrow("Width should be positive");
+    expect(() => calculateVolumeOfaPrism(width, length, height)).toThrow("Width should not be zero");
 
   });
+
+  test("should throw an error when height is zero", () => {
+    const width = 2;
+    const length = 3;
+    const height = 0;
+
+    expect(() => calculateVolumeOfaPrism(width, length, height)).toThrow("Height should not be zero");
+
+  });
+
 });
--- src/volume.ts
+++ src/volume.ts
@@ -15,6 +15,17 @@ export function calculateVolumeOfaPrism(
             throw new Error("Height should be positive");
         }
 
+        if (length == 0){
+            throw new Error("Length should not be zero");
+        }
+
+        if (width == 0){
+            throw new Error("Width should not be zero");
+        }
+
+        if (height == 0){
+            throw new Error("Height should not be zero");
+        }
 
     return width * length * height
 }
